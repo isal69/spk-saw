d85537d5aab3a252aeafc454ad685ca9572bf37f
isal69 <alqfais@gmail.com>
2022-11-29 19:41:49 +0700

    update

--- spk-saw-app/app/Views/layout/page_layout.php
+++ spk-saw-app/app/Views/layout/page_layout.php
@@ -6,7 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="<?= base_url('css/bootstrap.min.css'); ?>" rel="stylesheet">
-    <link href="<?= base_url('js/bootstrap.bundle.min.js'); ?>" rel="stylesheet">
+    <link href="<?= base_url('css/dataTables.bootstrap5.min.css'); ?>" rel="stylesheet">
+    
+
+    <script src="<?= base_url('js/jquery-3.5.1.js'); ?>"></script>
+    <script src="<?= base_url('js/bootstrap.min.js'); ?>"></script>
+    
 </head>
 
 <body>
@@ -14,7 +19,8 @@
 
     <?= $this->renderSection('content'); ?>
 
-    <script src="<?= base_url('js/bootstrap.bundle.min.js'); ?>"></script>
+    <script src="<?= base_url('js/jquery.dataTables.min.js'); ?>"></script>
+    <script src="<?= base_url('js/dataTables.bootstrap5.min.js'); ?>"></script>
 </body>
 
 </html>
