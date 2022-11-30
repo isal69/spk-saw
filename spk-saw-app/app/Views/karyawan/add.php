ebfc649688d6ebc638a4152b8fb06920d50f3aca
isal69 <alqfais@gmail.com>
2022-11-30 10:09:02 +0700

    update tambah data karyawan

--- spk-saw-app/app/Views/karyawan/add.php
+++ spk-saw-app/app/Views/karyawan/add.php
@@ -1,10 +1,8 @@
 <?= $this->extend('layout/page_layout') ?>
 <?= $this->section('content'); ?>
 
-<div class="container mt-3 mb-3">
-    <h1>Input Data Karyawan</h1>
-
-
+<div class="container-fluid">
+  
 </div>
 
 <?= $this->endSection(); ?>
--- spk-saw-app/app/Views/karyawan/index.php
+++ spk-saw-app/app/Views/karyawan/index.php
@@ -5,19 +5,68 @@
 
 <div class="container">
     <h1>Halaman data karyawan</h1>
-    <a href="<?=base_url('karyawan/add')?>" class="btn btn-primary">Tambah Data Karyawan Baru</a>
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Tambah Data Karyawan Baru
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Tambah Data Baru</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form method="post" action="<?php echo base_url('karyawan/ubah') ?>">
+                    <input type="hidden" name="id" value="">
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label>Nip</label>
+                            <input type="text" class="form-control form-control-sm" name="nip" placeholder="Nomor Induk Pegawai">
+                        </div>
+
+                        <div class="form-group">
+                            <label>Nama</label>
+                            <input type="text" class="form-control form-control-sm" name="nama" placeholder="Nama Lengkap">
+                        </div>
+
+                        <div class="form-group">
+                            <label>Jenis Kelamin</label>
+                            <select class="form-select form-select-sm" name="jekel" required>
+                                <option value="1">Laki-Laki</option>
+                                <option value="2">Perempuan</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Telepon</label>
+                            <input type="text" class="form-control form-control-sm" name="telepon" placeholder="Nomor Telepon" >
+                        </div>
+                        <div class="form-group">
+                            <label>Alamat</label>
+                            <textarea class="form-control form-control-sm" name="alamat" placeholder="Alamat Lengkap" style="resize: none;" ?></textarea>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                        <button type="button" class="btn btn-primary">Simpan</button>
+                    </div>
+            </div>
+        </div>
+    </div>
+    <a href="<?= base_url('karyawan/add') ?>" class="btn btn-primary">Tambah Data Karyawan Baru</a>
     <br><br>
     <div class="row">
         <div class="col">
             <table id="karyawan" class="table table-striped" style="width:100%">
                 <thead>
                     <tr>
-                        <th>Name</th>
-                        <th>Position</th>
-                        <th>Office</th>
-                        <th>Age</th>
-                        <th>Start date</th>
-                        <th>Salary</th>
+                        <th>NIP</th>
+                        <th>Nama</th>
+                        <th>Jenis Kelamin</th>
+                        <th>Telepon</th>
+                        <th>Alamat</th>
+                        <th>Aksi</th>
                     </tr>
                 </thead>
                 <tbody>
